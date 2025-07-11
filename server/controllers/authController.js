47f98b25cd9e1f9d26ce80fbc401bcdcb436335b
srahul0408 <rahul.sahani_cs22@gla.ac.in>
2025-07-11 09:34:57 +0530

    adding authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,5 @@
 import Users from "../models/userModel.js";
-import { hashString } from "../utils/index.js";
+import { compareString, createJWT, hashString } from "../utils/index.js";
 import { sendVerificationEmail } from "../utils/sendEmail.js";
 
 export const register = async(req, res, next) => {
@@ -37,3 +37,60 @@ export const register = async(req, res, next) => {
         res.status(404).json({ message: error.message });
     }
 }
+
+export const login = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        // validation
+        if (!email || !password) {
+            next("Please Provide User Credentials");
+            return;
+        }
+
+        // find user by email
+        const user = await Users.findOne({ email }).select("+password").populate({
+            path: "friends",
+            select: "firstName lastName location profileUrl -password",
+        });
+
+
+        if(!user) {
+            next("Invalid email or password");
+            return;
+        }
+
+
+        if (!user?.verified) {
+            next(
+                "User email is not verified. Check your email account and verify your email"
+            );
+            return;
+        }
+
+
+        // compare password
+        const isMatch = await compareString(password, user?.password);
+
+        if (!isMatch) {
+            next("Invalid email or password");
+            return;
+        }
+
+            user.password = undefined;
+
+            const token = createJWT(user?._id);
+        
+        res.status(201).json({
+            success: true,
+            message: "Login successfully",
+            user,
+            token,
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({ message: error.message });
+    }
+};
+
